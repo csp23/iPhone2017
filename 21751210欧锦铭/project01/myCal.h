fd2203afcbfe50e370a1a911c64c1d5703cb1bb2
ououou <ououou@JinmingdeMacBook-Pro.local>
2017-09-30 15:57:37 +0800

    feat: submit project01

---
+++ 21751210欧锦铭/project01/myCal.h
@@ -0,0 +1,18 @@
+//
+//  myCal.h
+//  myCalendar
+//
+//  Created by ououou on 2017/9/29.
+//  Copyright © 2017年 ououou. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+@interface myCal: NSObject
+
+- (void) printByYear:(int) year;
+- (void) printByMonth:(int) month AndYear:(int) year;
+
+@end
+
+//可读性更好的方法声明
+/* myCal_h */
